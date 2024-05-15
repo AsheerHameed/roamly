f4a4c05738dfebe67bb9e04daa8e33e54a99c04c
asheerhameed <asheerhameed54321@gmail.com>
2024-05-15 18:50:36 +0530

    landing page designed

--- src/app/common/roamly.module.ts
+++ src/app/common/roamly.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LandingModule } from '../modules/landing/landing.module';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -11,7 +13,13 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LandingModule,
+    MaterialModule
+  ],
+  exports:[
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class RoamlyModule { }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports:[
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+
   ]
 })
 export class MaterialModule { }
